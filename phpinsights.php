1516fd01e091ed5c7d23644fa936dbcae7ef3705
Paul <pvasilinenko@integrid.ru>
2020-10-22 21:25:41 +0300

    fix phpinsigths config

---
+++ phpinsights.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'preset' => 'default',
+    'exclude' => [
+        '.idea/',
+        '.docker/',
+        'vendor/',
+        'var/',
+    ],
+];
