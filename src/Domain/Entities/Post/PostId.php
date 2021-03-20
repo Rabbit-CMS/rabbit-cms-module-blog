20a9da05d9f9e5e11ad004da2881af08179c2457
Paul <pvasilinenko@integrid.ru>
2021-03-20 23:01:06 +0300

    add PostIdTest

--- src/Domain/Entities/Post/PostId.php
+++ src/Domain/Entities/Post/PostId.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace Paulmixxx\Blog\Domain\Entities\Post;
 
 use Ramsey\Uuid\Uuid;
+use Webmozart\Assert\Assert;
 
 final class PostId
 {
     private string $value;
 
+    /** @SuppressWarnings(PHPMD) */
     public function __construct(string $value)
     {
+        Assert::uuid($value);
         $this->value = $value;
     }
 
---
+++ tests/Unit/Domain/Entities/Post/PostIdTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Paulmixxx\Blog\Domain\Entities\Post;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+class PostIdTest extends TestCase
+{
+    /**
+     * @covers \Paulmixxx\Blog\Domain\Entities\Post\PostId::__construct
+     * @covers \Paulmixxx\Blog\Domain\Entities\Post\PostId::getValue
+     */
+    public function testCreateSuccess(): void
+    {
+        $postId = new PostId($uuid = '4c87589e-0dd5-4326-b009-f23089cd6df0');
+        self::assertEquals($uuid, $postId->getValue());
+    }
+
+    /**
+     * @covers \Paulmixxx\Blog\Domain\Entities\Post\PostId::__construct
+     */
+    public function testCreateFail(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new PostId($uuid = 'invalid-uuid');
+    }
+
+    /**
+     * @covers \Paulmixxx\Blog\Domain\Entities\Post\PostId::generate
+     */
+    public function testGenerate(): void
+    {
+        self::assertInstanceOf(PostId::class, PostId::generate());
+    }
+}
